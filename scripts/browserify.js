b67c25eda29b01776bc18890862509df594c731c
Julien Dargelos <julien.dargelos@me.com>
2017-11-13 10:57:58 +0100

    Clearing

--- scripts/browserify.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-
-fs.readFile('lib/index.js', (err, data) => {
-  if(err) throw err
-  data = data.toString().replace(/^export default /, '')
-  fs.writeFile("lib/html-util-element.js", data, function(err) {
-    if(err) throw err
-  })
-})
